872bc15bdd43b45e8c0830c9b34c53bd9e7b720f
Xu Chunyang <xuchunyang56@gmail.com>
2023-12-03 17:12:57 +0800

    Update queue text

--- resources/js/Pages/StartQueueForTesting.vue
+++ resources/js/Pages/StartQueueForTesting.vue
@@ -4,7 +4,7 @@ import { Link } from "@inertiajs/vue3";
 
 <template>
     <div>
-        <h1 class="text-xl font-medium">启用 Queue</h1>
+        <h1 class="text-xl font-medium">测试 Queue 任务</h1>
         <p class="text-sm my-2 opacity-75">
             用来测试 Pulse 的「Queues」和「Slow Jobs」
         </p>
@@ -16,7 +16,7 @@ import { Link } from "@inertiajs/vue3";
                 :href="route('relax-and-have-fun')"
                 class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"
             >
-                任务开始
+                开始任务
             </Link>
         </div>
     </div>
