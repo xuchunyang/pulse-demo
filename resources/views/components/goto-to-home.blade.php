716b3f87d28f0e761b617a4cd0a41f1689b797ca
Xu Chunyang <xuchunyang56@gmail.com>
2023-12-04 21:30:54 +0800

    Load tailwind css via play cdn

--- resources/views/components/goto-to-home.blade.php
+++ resources/views/components/goto-to-home.blade.php
@@ -1,3 +1,7 @@
 <div class="col-span-full">
     <a href="/" class="leading-none text-sm dark:text-gray-400 hover:text-blue-600">返回网站</a>
 </div>
+
+@pushOnce('scripts')
+    <script src="https://cdn.tailwindcss.com"></script>
+@endPushOnce
